500c18dfb79e90f2e2430cce887d769c074c1556
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-18 20:59:05 +0530

    Flip coin

---
+++ Day5Problems/Day5Problems/Program.cs
@@ -0,0 +1,21 @@
+﻿int NumberOftimes = 5;
+int heads = 0, tails = 0;
+double hPercentage, tPercentage;
+Random random = new Random();
+for (int i = 0; i < NumberOftimes; i++)
+{
+    int coinFlip = random.Next(0, 2);
+    if (coinFlip < 0.5)
+    {
+        Console.WriteLine(" Tails");
+        tails++;
+    }
+    else
+    {
+        Console.WriteLine(" Heads");
+        heads++;
+    }
+}
+hPercentage = 100 * heads / NumberOftimes;
+tPercentage = 100 * tails / NumberOftimes;
+Console.WriteLine("Heads % : " + hPercentage + " vs Tails % : " + tPercentage);
